95a62c4c38442bdc2f6a2e53656ccfb773599343
Pratik Umesh Thorat <pratikthorat195@gmail.com>
2024-02-09 13:18:02 +0530

    new code

---
+++ JavaScript_Practice_codes/Javascript1.js
@@ -0,0 +1,6 @@
+//Coding challenge #1: Print numbers 1 to 10 
+
+for(var i=1; i<=10 ; i++)
+{
+    console.log(i);
+}
---
+++ JavaScript_Practice_codes/Javascript2.js
@@ -0,0 +1,6 @@
+//Print the odd numbers less than 100
+
+for(var i=1; i<=100; i= i+2)
+{
+    console.log(i);
+}
